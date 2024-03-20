10e9ced9d8e4f4baa136c208d9078019b859ebad
JonathanStoll <jonafstoll@gmail.com>
2024-03-19 22:57:43 -0300

    creating users

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { useState } from "react";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [loading, setLoading] = useState(false);
+
+  async function signupHandeler({ email, password }) {
+    setLoading(true);
+    await createUser(email, password);
+    setLoading(false);
+  }
+
+  if (loading) {
+    return <LoadingOverlay message={'creating user...'} />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandeler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 const API_KEY = "AIzaSyDS9KxqTIP1gjK-dxjy6Zv4QxxpYjWDgSQ";
-async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=" +
-      API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+
+export async function createUser(email, password) {
+  try {
+    const response = await axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+    console.log("User created successfully:", response.data);
+    return response.data; // Return the response data if needed
+  } catch (error) {
+    console.error("Error creating user:", error.response.data.error.message);
+    throw new Error(error.response.data.error.message);
+  }
 }
