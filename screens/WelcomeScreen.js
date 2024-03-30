becacc1fe46a952651ded532c7636c9064420c14
JonathanStoll <jonafstoll@gmail.com>
2024-03-30 20:17:03 -0300

    authenticate a user to fetch data

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState('')
+  const authCtx = useContext(AuthContext)
+  const token = authCtx.token
+  useEffect(()=>{
+    axios.get(`https://react-native-course-ad9c2-default-rtdb.firebaseio.com/message.json?auth=${token}`)
+    .then((response)=>{
+      console.log(response.data)
+      setMessage(response.data)
+    })
+  }, [token])
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
